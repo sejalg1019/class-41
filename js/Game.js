29abf302265089f1275f53a2f102e5a31cf5de97
sejalg1019 <sejalg1019@gmail.com>
2020-12-22 19:16:42 -0500

    added-files

--- js/Game.js
+++ js/Game.js
@@ -93,6 +93,8 @@ class Game {
 
     if(player.distance > 3860){
       gameState = 2;
+      player.rank += 1;
+      Player.updateCarsAtEnd(player.rank);
     }
    
     drawSprites();
@@ -100,5 +102,6 @@ class Game {
 
   end(){
     console.log("Game Ended");
+    console.log(player.rank);
   }
 }
